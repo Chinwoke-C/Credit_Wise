8120da67bdccb27c4ca99edd1bfa82f6dc580228
chinwoke <canugwara99@gmail.com>
2023-07-01 14:53:49 +0100

    first commit

---
+++ src/main/java/com/loan/credit_wise/CreditWiseApplication.java
@@ -0,0 +1,13 @@
+package com.loan.credit_wise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditWiseApplication.class, args);
+	}
+
+}
