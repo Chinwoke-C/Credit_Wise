c97c1575de18f68a2bcb5b75608e252e5805b399
chinwoke <canugwara99@gmail.com>
2023-07-04 05:53:25 +0100

    updated security package

--- src/main/java/com/loan/credit_wise/auth/user/service/AppTokenServiceImpl.java
+++ src/main/java/com/loan/credit_wise/auth/user/service/AppTokenServiceImpl.java
@@ -1,4 +1,14 @@
 package com.loan.credit_wise.auth.user.service;
 
-public class AppTokenServiceImpl {
+import com.loan.credit_wise.auth.user.data.models.AppToken;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AppTokenServiceImpl implements AppTokenService{
+    @Override
+    public void saveToken(AppToken token) {
+
+    }
 }
